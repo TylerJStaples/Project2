ec80ca9a0282f1b7d396ed0c267fa037a9379b4c
taw1313 <taw1313@gmail.com>
2018-06-25 13:41:06 -0400

    TAW - Quick test of branching... server can now route main.html

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 //routes
-require("./routes/html-routes.js");
+require("./routes/html-routes.js")(app);
 
-db.sequelize.sync().then(function() {
+// db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
-});
+// });
