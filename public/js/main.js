144c3c5b270e9843ba1689abb37695057e9dbffc
buchananc <candacebuchanan7@gmail.com>
2018-06-28 21:18:56 -0400

    fixed main.js page to work

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,4 @@
-// Initialize Firebase (config & env didn't work, will switch keys once app is working)
-// var apiKey = config.apiKey;
-// var authDomain = config.authDomain;
-// var databaseURL = config.databaseURL;
-// var projectId = config.projectId;
-// var storageBucket = config.storageBucket;
-// var messagingSenderId = config.messagingSenderId;
-
-/* Candace's
+//Candace's
 var config = {
     apiKey: "AIzaSyA2_tnNsCgwOh6gNQIhuBPu5dzrtdctTEU",
     authDomain: "in-a-pinch-project-2.firebaseapp.com",
@@ -15,30 +7,11 @@ var config = {
     storageBucket: "in-a-pinch-project-2.appspot.com",
     messagingSenderId: "674290992138"
 };
-*/
-/* Todd's
-*/
-var config = {
-    apiKey: "AIzaSyDUWLn9TlTiPbRUKh0lypHoKXeSQSG_FUs",
-    authDomain: "inapinch-1529857093090.firebaseapp.com",
-    databaseURL: "https://inapinch-1529857093090.firebaseio.com",
-    projectId: "inapinch-1529857093090",
-    storageBucket: "inapinch-1529857093090.appspot.com",
-    messagingSenderId: "600057660668"
-};
 
 firebase.initializeApp(config);
 // Get a reference to the database service
-// var database = firebase.database(); //added
 const auth = firebase.auth();
-const usersRef = firebase.database().ref("/users");
-///////////////////////////////////////////////////////
-// How Firebase Database is Set Up
-///////////////////////////////////////////////////////
-// /users
-//     /userId1
-//        userEmail: "123email@gmail.com"
-//        userName: "Shally"
+// const usersRef = firebase.database().ref("/users");
 
 $(document).ready(function () {
 ///////////////////////////////////////////////////////
@@ -59,41 +32,35 @@ $(document).ready(function () {
 //Add login event
 ///////////////////////////////////////////////////////
 
-    btnLogin.addEventListener('click', e => {
-        // Get email and pass
-        const email = userEmail.value;
-        const pass = userPassword.value;
+btnLogin.addEventListener('click', e => {
+    e.preventDefault();
+    // Get email and pass
+    const email = userEmail.value;
+    const pass = userPassword.value;
 
-        console.log(email, pass);
+    console.log(email, pass);
 
-        // Sign in
-        const promise = auth.signInWithEmailAndPassword(email, pass);
-        promise
-            .then( user => {
-                /*
-                    */
-            })
-            .catch(function (error) {
-                // Handle error
-                var errorCode = error.code;
-                var errorMessage = error.message;
+    // Sign in
+    auth.signInWithEmailAndPassword(email, pass)
+        // .then( user => {
+        //     console.log("I made it this far!");
+        //     document.getElementById("user_para").innerHTML = "Welcome User: " + user.displayName;
+        // })
+        // .catch(function (error) {
+        //     // Handle error
+        //     var errorCode = error.code;
+        //     var errorMessage = error.message;
 
-                $("#exampleModal").modal();
-                // alert("You look a little flushed! The email you entered is not correct. Try again!");
-                console.log(errorMessage);
-                alert(errorMessage);
-            });
-    });
+        //     $("#exampleModal").modal();
+        //     // alert("You look a little flushed! The email you entered is not correct. Try again!");
+        //     console.log(errorMessage);
+        //     alert(errorMessage);
+        // });
+});
 
 ///////////////////////////////////////////////////////
 // Add login to signup event
 ///////////////////////////////////////////////////////
-    // sign-up.addEventListener('click', e => {
-    //     $('.log-section').addClass('hide');
-    //     $('#signupDiv').fadeIn('slow').removeClass('hide');
-    // });
-
-
     $('#sign-up').click(function () {
         $('.log-section').hide();
         $('#signupDiv').show("slow");
@@ -105,42 +72,32 @@ $(document).ready(function () {
 ///////////////////////////////////////////////////////
     $('#createUser').click( function() {
         // Get username, email, and pass
-        const username = txtUsername.value.trim(); //added, not working yet
+        const displayName = txtUsername.value.trim(); //added, not working yet
         const email = txtEmail.value;
         const pass = txtPassword.value;
-        // store this locally
-        localStorage.setItem('user', JSON.stringify(username));
-        // TAW  const auth = firebase.auth();
 
-        // Sign in
-        const promise = auth.createUserWithEmailAndPassword(email, pass);
-        promise
-            .then( function(user) {
-                // write additional child data to of the user
-                console.log(`update() ${username}`)
-                console.log(`update() ${user.user.uid}`)
-                usersRef.child(user.user.uid).update({'userName': username});
+
+        // Sign up
+        auth.createUserWithEmailAndPassword(email, pass)
+            .then(function(userCredential) {
+                userCredential.user.updateProfile({
+                    displayName: displayName
+                }).then( function() {
+                    console.log(userCredential);
+                    //if redirecting to /map the line below can be taken out
+                document.getElementById("user_para").innerHTML = "Welcome User: " + userCredential.user.displayName;
+                ('#signupDiv').modal('hide');  ///TODO: doesnt hide modal 
+                })
             })
             .catch( function(e) {
               console.log(`error from createUserWithEmailAndPassword() ${e.message}`);
-            }) ;
+            });
+
     });
 
-    // btnRegister.addEventListener('click', e => {
-    //     // Get username, email, and pass
-    //     // const username = txtUsername.value; //added, not working yet
-    //     const email = txtEmail.value;
-    //     const pass = txtPassword.value;
-    //     const auth = firebase.auth();
-
-    //     // Sign in
-    //     const promise = auth.createUserWithEmailAndPassword(email, pass);
-    //     promise.catch(e => console.log(e.message));
-    // });
-
-    btnLogout.addEventListener('click', e => {
-        auth.signOut();
-    });
+btnLogout.addEventListener('click', e => {
+    auth.signOut();
+});
 
 
 ///////////////////////////////////////////////////////
@@ -149,40 +106,14 @@ $(document).ready(function () {
 
     auth.onAuthStateChanged(function (user) {
         console.log(user);
-        //saves user to local storage
-        var username = JSON.parse(localStorage.getItem('user'));
 
         if (user) {
-
-            //still trying to connect firebase database
-            // firebaseDataBase.ref('users/' + user.uid).set({
-            //     username: username
-            // });
-            // var retrievedUserName = firebase.database().ref('/users/' + user.uid).once('value').then(function(snapshot) {
-            // var username = snapshot.val().username;
 
             // User is signed in.
             document.getElementById("user_div").style.display = "block";
             document.getElementById("main_div").style.display = "none";
-            // document.getElementById("user_para").innerHTML = "Welcome User: " + username;
-            
-
-            // TAW - the user is already known
-
-            //var user = firebase.auth().currentUser;
-
-            var email_id = user.email;
-            document.getElementById("user_para").innerHTML = "Welcome User: " + email_id;
-            const promise = usersRef.child(user.uid).once('value');
-            promise
-                .then(snap => {
-                    username = snap.val().userName;
-                    console.log(`user ${username} logged in`);
-                    window.location.href = "/map";
-                })
-                .catch(err => {
-                    console.log(`user name does not exist for ${emai}`);
-                });
+            document.getElementById("user_para").innerHTML = "Welcome User: " + user.displayName;
+            window.location.assign('/map');
         } 
         else {
 
